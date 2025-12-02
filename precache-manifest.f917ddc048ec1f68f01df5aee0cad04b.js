dee5c165ff15870cf58c358c326ac4787110a7ef
DiepKhai <aicungduoc49@gmail.com>
2025-12-02 21:30:51 +0700

    Updates

---
+++ precache-manifest.f917ddc048ec1f68f01df5aee0cad04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12616dad09bc8ebf84d62ccfae19f395",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "e05b616bdb76dc3730d0",
+    "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2a82bb5358e364a7a612",
+    "url": "/my-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "e05b616bdb76dc3730d0",
+    "url": "/my-portfolio/static/js/2.c0f2d267.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-portfolio/static/js/2.c0f2d267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a82bb5358e364a7a612",
+    "url": "/my-portfolio/static/js/main.34e7c53b.chunk.js"
+  },
+  {
+    "revision": "28d97e6c4183a7c87d51",
+    "url": "/my-portfolio/static/js/runtime-main.9994cf37.js"
+  }
+]);
